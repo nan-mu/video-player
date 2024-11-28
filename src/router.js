69e4e9c7ea388eb0e305fbf1752a1cb8a32084bd
nan mu <mu.nan.11@outlook.com>
2024-11-28 23:31:57 +0800

    fix: 遗留的错误路由

--- src/router.js
+++ src/router.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import VideoList from './components/VideoList.vue'
-import VideoPlayer from './components/VideoPlayer.vue'
+import App from './App.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: VideoList },
-  { path: '/player/:id', name: 'player', component: VideoPlayer }
+  { path: '/', name: 'home', component: App },
+//   { path: '/player/:id', name: 'player', component: VideoPlayer }
 ]
 
 const router = createRouter({
